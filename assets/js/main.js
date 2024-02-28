6fa3e6c061f34923d5e68efce66edc6ec6ad967d
RiccardoImperiale <riccardoimperiale.dev@gmail.com>
2024-02-28 15:34:33 +0100

    fix style

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,22 @@
 const gameBoard = document.querySelector('.game_board');
 const startBtn = document.querySelector('button');
 
-// click on a button and generate a square grid  
-// each cell has a progressive number, from 1 to 100
 startBtn.addEventListener('click', () => {
+    // generate a square grid  
     for (let i = 1; i <= 100; i++) {
+        // each cell has a progressive number, from 1 to 100
         const squareMarkup = `<div class="square">${i}</div>`;
         gameBoard.style.border = '2px solid var(--cm-primary-darker)'
         gameBoard.insertAdjacentHTML('beforeend', squareMarkup)
     }
 
+    // for each cell on click change color
     const squares = document.querySelectorAll('.square');
-
     for (let i = 0; i < squares.length; i++) {
-        squares[i].addEventListener('click', (e) => {
-            e.target.classList.toggle('bg_lightblue')
+        let square = squares[i];
+        square.addEventListener('click', () => {
+            square.classList.toggle('square_dark');
+            console.log(square.innerText);
         })
     }
 
